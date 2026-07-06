861a4c134fc5fdde61b5170bf9ab7655e21202dc
diepphungquocan-jpg <diepphungquocan@gmail.com>
2026-07-06 18:22:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from google import genai
 from google.genai import types
 
-API_KEY = "AQ.Ab8RN6KR07QpAL1aZTXukygIZfjFiZeLfuI372OHV8-apuqEHw"
+API_KEY = "AQ.Ab8RN6K39wg4oBXOZIrZiR2en1ae22arwAfC9_AArzazpt7INg"
 client = genai.Client(api_key=API_KEY)
 
 # Giao diện
